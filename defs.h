3cd43883a71cab6936a68a28885a707c6930e311
mattwryan <wieserma93@gmail.com>
2023-03-27 15:20:09 -0400

    Added bitboard file, added ability to place pawns on the board

--- defs.h
+++ defs.h
@@ -212,6 +212,7 @@ typedef struct
 /* MACROS */
 
 #define FR2SQ(f, r) ((21 + (f)) + ((r)*10))
+#define SQ64(sq120) Sq120ToSq64[sq120]
 
 /* GLOBALS */
 
@@ -220,8 +221,10 @@ extern int Sq64ToSq120[64];
 
 /* FUNCTIONS */
 
+// init.c
 extern void AllInit();
 
-// init.c
+// bitboards.c
+extern void PrintBitBoard(U64 bb);
 
 #endif
--- vice.c
+++ vice.c
@@ -9,6 +9,19 @@ int main()
 {
  AllInit();
 
+ U64 playBitBoard = 0ULL;
+
+ printf("Start:\n\n");
+ PrintBitBoard(playBitBoard);
+
+ playBitBoard |= (1ULL << SQ64(D2));
+ printf("D2 Added:\n\n");
+ PrintBitBoard(playBitBoard);
+
+ playBitBoard |= (1ULL << SQ64(G2));
+ printf("G2 Added:\n\n");
+ PrintBitBoard(playBitBoard);
+
  int index = 0;
 
  for (index = 0; index < BRD_SQ_NUM; ++index)
